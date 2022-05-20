e23660f3f4151042f172efbd7e2444d87134f9f3
Patrick Niyogitare <patrickniyogitare28@gmail.com>
2022-05-20 22:55:14 +0200

    chore(config): adde theme

---
+++ src/theme/colors.ts
@@ -0,0 +1,14 @@
+export const colors = {
+    primaryColor: "#DA18A3",
+    redLinearPrimaryColor: "#EB1484",
+    redLinearSecondaryColor: "#C81CC5",
+    switchColor: '#24252D',
+    white: '#FFFFFF',
+    grey1: '#E3E1E3',
+    grey2: '#888888',
+    grey3: '#4F4F4F',
+    black1: '#2D2E36',
+    black2: '#1B1A21',
+    black3: '#E3E1E3',
+    black4:'#24252D'
+}
